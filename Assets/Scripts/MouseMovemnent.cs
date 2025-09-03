3ba07f513c388e5246614a1ccb92432b5f11615a
Yung-Modi <chrisreaper404@gmail.com>
2025-09-03 12:37:49 -0500

    Found tutorial, trying that

---
+++ Assets/Scripts/MouseMovemnent.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Locking cursor to the middle of the screen and making it invisible
+      Cursor.lockState = CursorLockMode.Locked;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        
+
+
+
+
+
+    }
+}
