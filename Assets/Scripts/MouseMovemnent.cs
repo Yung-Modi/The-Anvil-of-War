c2dabfe9b039b3e310dfd1a0a7a992de2ade9fcb
Yung-Modi <chrisreaper404@gmail.com>
2025-09-05 15:14:35 -0500

    end of 9/5/25

--- Assets/Scripts/MouseMovemnent.cs
+++ Assets/Scripts/MouseMovemnent.cs
@@ -3,26 +3,36 @@ using UnityEngine;
 public class MouseMovement : MonoBehaviour
 {
 
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 500f;
 
     float xRotation = 0f;
     float yRotation = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
     void Start()
     {
         //Locking cursor to the middle of the screen and making it invisible
       Cursor.lockState = CursorLockMode.Locked;  
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-     
-        
+      //Getting the mouse inputs
+      float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+      float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // Rotation around the x axis (Up and Down)
+        xRotation -= mouseY;
 
+        // Clamp the rotation
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
 
+        // Rotation around the y axis (Left and Right)
+        yRotation += mouseX;
 
-
+        // Apply rotations to our transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHieght = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    bool isMoving;
+
+    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+      // Ground check
+      isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+
+
+
+
     }
 }
